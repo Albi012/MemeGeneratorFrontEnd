e504029f5af657097bd5da9a0b955e917561b5c6
David Korpos <korposdavid@gmail.com>
2019-12-17 14:57:14 +0100

    init createnewmeme and leaderboard components

---
+++ src/components/CreateNewMeme.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+interface Props {
+    
+}
+interface State {
+    
+}
+
+class CreateNewMeme extends Component<Props, State> {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                <p>Create new</p>
+            </div>
+        )
+    }
+}
+
+export default CreateNewMeme
---
+++ src/components/Leaderboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const Leaderboard: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>leaderboard</h1>
+    </div>
+  );
+};
+
+export default Leaderboard;
