1b159cb6fc6923e9e314157eb11a516bcb732c37
David Korpos <korposdavid@gmail.com>
2019-12-18 10:01:31 +0100

    init MemeList component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Header from "./layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Leaderboard from "./components/Leaderboard"
 import CreateNewMeme from "./components/CreateNewMeme"
+import MemeList from "./components/MemeList"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Route path="/" component={MemeList}/>
         <Route path="/leaderboard" component={Leaderboard} />
         <Route path="/create" component={CreateNewMeme} />
       </div>
---
+++ src/components/MemeList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const MemeList: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>meme list</h1>
+    </div>
+  );
+};
+
+export default MemeList;
