7adeea5b08df9ed0cd483c974abdf8114e87b149
David Korpos <korposdavid@gmail.com>
2019-12-18 13:47:36 +0100

    add buttons to MemeCard

--- src/components/MemeCard.tsx
+++ src/components/MemeCard.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import MemeModel from "../models/MemeModel";
 import Card from "react-bootstrap/Card";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+import Button from "react-bootstrap/Button";
 
 interface Props {
   meme: MemeModel;
 }
 
-const MemeCard: React.FC<Props> = (props) => {
+const MemeCard: React.FC<Props> = props => {
   return (
     <Card>
-      <Card.Body>{props.meme.url}</Card.Body>
+      <Card.Img variant="top" src={props.meme.url} />
+      <Card.Body>
+        <Button variant="success">
+          <FaArrowUp />
+          ({props.meme.upVote})
+        </Button>
+        <Button variant="danger">
+          <FaArrowDown />
+          ({props.meme.downVote})
+        </Button>
+      </Card.Body>
     </Card>
   );
 };
--- src/components/MemeList.tsx
+++ src/components/MemeList.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import MemeModel from "../models/MemeModel";
 import MemeCard from "./MemeCard";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 interface Props {
   memes: MemeModel[];
@@ -8,11 +11,15 @@ interface Props {
 
 const MemeList: React.FC<Props> = (props: Props) => {
   return (
-    <div>
+    <Container>
       {props.memes.map((meme: MemeModel) => (
-        <MemeCard meme={meme} />
+        <Row className="justify-content-md-center">
+          <Col md={6}>
+            <MemeCard meme={meme} />
+          </Col>
+        </Row>
       ))}
-    </div>
+    </Container>
   );
 };
 
